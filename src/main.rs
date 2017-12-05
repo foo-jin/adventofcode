f60f60c7136981d9c3d04e14ce1fe607a80fc7a2
Frank <frank.049@hotmail.com>
2017-12-05 06:49:24 +0100

    Day 5

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 extern crate adventofcode;
 
-use adventofcode::seventeen::d4;
+use adventofcode::seventeen::d5;
 use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
-    let filename = "./data/2017/d4-input";
+    let filename = "./data/2017/d5-input";
     let mut f = File::open(filename)
         .expect("error opening the file");
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("error reading the file");
 
-    let result = d4::check_anagram(&contents);
+    let result = d5::escape_maze(&contents);
     println!("Result: {}", result)
 }
---
+++ src/seventeen/d5.rs
@@ -0,0 +1,32 @@
+
+pub fn escape_maze(input: &str) -> u32 {
+    let mut lines: Vec<i32> = input
+        .lines()
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
+
+    let n = lines.len();
+    let mut i = 0;
+    let mut j = 0;
+    while i >= 0 && i < n as i32 {
+        let el = &mut lines[i as usize];
+        i = i + *el;
+        if (*el) >= 3 {
+            *el -= 1;
+        } else {
+            *el += 1;
+        }
+        j += 1;
+    }
+    j
+}
+
+#[cfg(test)]
+mod tests {
+    use seventeen::d5::*;
+
+    #[test]
+    fn test_escape_maze1() {
+        assert_eq!(escape_maze("0\n3\n0\n1\n-3"), 10);
+    }
+}
