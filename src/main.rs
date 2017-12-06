174653322abbed09d58c620b60639a1ba1e56041
Frank <frank.049@hotmail.com>
2017-12-06 17:43:06 +0100

    Day 6

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 extern crate adventofcode;
 
-use adventofcode::seventeen::d5;
+use adventofcode::seventeen::d6;
 use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
-    let filename = "./data/2017/d5-input";
+    let filename = "./data/2017/d6-input";
     let mut f = File::open(filename)
         .expect("error opening the file");
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("error reading the file");
 
-    let result = d5::escape_maze(&contents);
+    let result = d6::redistribute(&contents);
     println!("Result: {}", result)
 }
---
+++ src/seventeen/d6.rs
@@ -0,0 +1,49 @@
+use std::collections::HashMap;
+
+
+pub fn redistribute(input: &str) -> u32 {
+    let mut input: Vec<u32> = input
+        .split_whitespace()
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect();
+
+    let n = input.len();
+    let mut i = 0;
+    let mut seen = HashMap::new();
+
+    loop {
+        let mut clone = input.clone();
+        let (mut key, el) = clone
+            .iter_mut()
+            .enumerate()
+            .rev()
+            .max_by_key(|&(k, &mut v)| v)
+            .unwrap();
+        
+        input[key] = 0;
+        key += 1;
+        while *el > 0 {
+            input[key % n] += 1;
+            key += 1;
+            *el -= 1;
+        }
+
+        i += 1;
+
+        let cpy = input.clone();
+        if let Some(x) = seen.insert(cpy, i) {
+            return i - x;
+        }
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use seventeen::d6::*;
+
+    #[test]
+    fn test_redistribute() {
+        assert_eq!(redistribute("0\t2\t7\t0"), 4);
+    }
+}
