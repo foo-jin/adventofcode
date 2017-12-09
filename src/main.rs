d95c397b91e54cd2859f4592311bbe93e6046d00
Frank <frank.049@hotmail.com>
2017-12-09 06:38:08 +0100

    Day 9

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 extern crate adventofcode;
 
-use adventofcode::seventeen::d8;
+use adventofcode::seventeen::d9;
 use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
-    let filename = "./data/2017/d8-input";
-    let mut f = File::open(filename)
-        .expect("error opening the file");
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("error reading the file");
+    let filename = "./data/2017/d9-input";
+    let mut f = File::open(filename).expect("error opening the file");
 
-    let result = d8::largest_register(&contents);
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect(
+        "error reading the file",
+    );
+
+    let result = d9::count_groups(&contents);
     println!("Result: {}", result)
 }
---
+++ src/seventeen/d9.rs
@@ -0,0 +1,77 @@
+
+pub fn count_groups(input: &str) -> u32 {
+    // let mut nesting = 0;
+    let mut count = 0;
+    let mut chars = input.chars();
+
+    while let Some(c) = chars.next() {
+        match c {
+            '!' => {
+                chars.next();
+            }
+            '<' => {
+                while let Some(c) = chars.next() {
+                    match c {
+                        '!' => {chars.next();},
+                        '>' => break,
+                        _ => count += 1,
+                    }
+                    //println!("ignoring: {}", c);
+                }
+            }
+            '{' => {
+                //nesting += 1;
+                //count += nesting;
+                },
+            '}' => (),//nesting -= 1,
+            ',' => (),
+            c => {
+                println!("unexpected input:{}", c);
+                panic!("unexpected input!");
+            }
+        }
+    }
+
+    count
+}
+
+#[cfg(test)]
+mod tests {
+    use seventeen::d9::*;
+
+
+    #[test]
+    fn test_count_groups1() {
+        assert_eq!(count_groups("<>"), 0);
+    }
+
+    #[test]
+    fn test_count_groups2() {
+        assert_eq!(count_groups("<random characters>"), 17);
+    }
+
+    #[test]
+    fn test_count_groups3() {
+        assert_eq!(count_groups("<{o\"i!a,<{i<a>"), 10);
+    }
+
+    #[test]
+    fn test_count_groups4() {
+        assert_eq!(count_groups("<<<<>"), 3);
+    }
+
+    #[test]
+    fn test_count_groups5() {
+        assert_eq!(count_groups("<{!>}>"), 2);
+    }
+
+    #[test]
+    fn test_count_groups6() {
+        assert_eq!(count_groups("<!!>"), 0);
+    }
+
+    #[test]
+    fn test_count_groups7() {
+        assert_eq!(count_groups("<!!!>>"), 0);
+    }
+}
