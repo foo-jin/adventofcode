e3fb4e6d3cf5a6cb9c0e3b5ccfe811c3973defe0
Frank <frank.049@hotmail.com>
2017-12-12 07:18:35 +0100

    Day 12

--- src/seventeen/d12.rs
+++ src/seventeen/d12.rs
@@ -3,34 +3,48 @@
 
 pub fn pipegraph(input: &str) -> u32 {
     let mut graph = HashMap::new();
-    let mut connected = HashSet::new();
-
+    let mut len = 0;
     for l in input.trim().lines() {
+        len += 1;
         let mut it = l.split_whitespace();
-        let first: u32 = it.next().unwrap().parse().unwrap();
+        let key: u32 = it.next().unwrap().parse().unwrap();
         let _ = it.next();
 
-        let mut isconn = connected.contains(&first);
-        let mut last = Vec::new();
+        let mut neigh = Vec::new();
         for s in it {
             let k: u32 = s.chars().filter(|&c| c != ',').collect::<String>().parse().unwrap();
-            if isconn {
-                connected.insert(k);
-                println!("inserting {}", k);
-            } else if connected.contains(&k) {
-                isconn = true;
-
-                for n in last.clone() {
-                    println!("inserting {}", n);
-                    connected.insert(n);
-                }
-            } else {
-                last.push(k);
-            }
+            neigh.push(k);
         }
+        graph.insert(key, neigh);
     }
 
-    connected.len() as u32
+    let mut components = Vec::new();
+    for i in 0 .. len {
+        if let None = graph.get(&i) {
+            continue;
+        }
+
+        let mut stack = Vec::new();
+        stack.push(i);
+        let mut connected = HashSet::new();
+        while !stack.is_empty() {
+            let cur = stack.pop().unwrap();
+            if connected.contains(&cur) {
+                continue
+            } else {
+                connected.insert(cur);
+                if let Some(ks) = graph.get(&cur) {
+                    for &k in ks {
+                        stack.push(k);
+                    }
+                }
+                
+            }
+            graph.remove(&cur);
+        }
+        components.push(connected);
+    }    
+    components.len() as u32
 }
 
  #[cfg(test)]
@@ -41,6 +55,6 @@ mod tests {
     fn test_pipegraph1() {
         let input = "0 <-> 2\n1 <-> 1\n2 <-> 0, 3, 4\n3 <-> 2, 4\n4 <-> 2, 3, 6\n5 <-> 6\n6 <-> 4, 5";
 
-        assert_eq!(pipegraph(input), 6);
+        assert_eq!(pipegraph(input), 2);
     }
 }
