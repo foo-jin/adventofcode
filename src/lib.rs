8bb09dab65ad75389f4b3e8862c949dccd5b6b56
Frank <frank.049@hotmail.com>
2017-12-20 21:47:49 +0100

    Refactor day 20 Add benchmarks for day 20

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 #![feature(i128_type)]
 #![feature(inclusive_range_syntax)]
 #![feature(match_default_bindings)]
+#![feature(test)]
 
 #[macro_use]
 extern crate nom;
 extern crate failure;
 extern crate bit_vec;
 extern crate rayon;
+extern crate test;
 
 pub mod sixteen;
 pub mod seventeen;
--- src/seventeen/d20.rs
+++ src/seventeen/d20.rs
@@ -1,5 +1,8 @@
-use failure::Error;
 use std::ops::AddAssign;
+use std::cmp::Ordering;
+
+use failure::Error;
+
 
 #[derive(Debug, Eq, PartialEq, Clone, Copy)]
 struct Vector {
@@ -12,16 +15,38 @@ impl Vector {
     fn from_str(input: &str) -> Result<Vector, Error> {
         let brackets: &[char] = &['<', '>'];
         let mut it = input.trim_matches(brackets).trim().split(',');
+
         let x = it.next().unwrap().parse()?;
         let y = it.next().unwrap().parse()?;
         let z = it.next().unwrap().parse()?;
+
         Ok(Vector { x, y, z })
+    }
+
+    fn sum(&self) -> usize {
+        (self.x.abs() + self.y.abs() + self.z.abs()) as usize
     }
 }
 
 impl AddAssign for Vector {
     fn add_assign(&mut self, rhs: Vector) {
-        *self = Vector{ x: self.x + rhs.x, y: self.y + rhs.y, z: self.z + rhs.z }
+        *self = Vector {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl Ord for Vector {
+    fn cmp(&self, rhs: &Vector) -> Ordering {
+        self.sum().cmp(&rhs.sum())
+    }
+}
+
+impl PartialOrd for Vector {
+    fn partial_cmp(&self, rhs: &Vector) -> Option<Ordering> {
+        Some(self.cmp(rhs))
     }
 }
 
@@ -52,10 +77,6 @@ impl Particle {
         self.vel += self.acc;
         self.pos += self.vel;
     }
-
-    fn eq_pos(&self, rhs: Particle) -> bool {
-        self.pos == rhs.pos
-    }
 }
 
 fn parse(input: &str) -> Result<Vec<Particle>, Error> {
@@ -67,12 +88,8 @@ fn first(input: &str) -> Result<usize, Error> {
     let particles = parse(input)?;
     let result = particles
         .iter()
-        .map(|p| {
-            let Vector { x, y, z } = p.acc;
-            (x.abs() + y.abs() + z.abs()) as usize
-        })
         .enumerate()
-        .min_by(|(_,x), (_, y)| x.cmp(y))
+        .min_by(|(_, &x), (_, &y)| x.acc.cmp(&y.acc))
         .map(|(i, _)| i)
         .unwrap();
     Ok(result)
@@ -80,13 +97,16 @@ fn first(input: &str) -> Result<usize, Error> {
 
 fn second(input: &str) -> Result<usize, Error> {
     let mut particles = parse(input)?;
+    
     for _ in 0..1000 {
         let aux = particles.clone();
-        particles.retain(|p| !aux.iter().any(|p2| p != p2 && p.eq_pos(*p2)));
+        particles.retain(|p| !aux.iter().any(|other| p != other && p.pos == other.pos));
+
         for p in particles.iter_mut() {
-            p.update();
+            p.update()
         }
     }
+
     Ok(particles.len())
 }
 
@@ -97,9 +117,13 @@ pub fn run(input: &str) -> Result<usize, Error> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fmt::Debug;
 
-    fn check_first(input: &str, expected: usize) {
-        match first(input) {
+    fn check<T>(result: Result<T, Error>, expected: T)
+    where
+        T: PartialEq + Eq + Debug,
+    {
+        match result {
             Ok(result) => {
                 assert_eq!(result, expected);
             }
@@ -112,18 +136,12 @@ mod tests {
         };
     }
 
+    fn check_first(input: &str, expected: usize) {
+        check(first(input), expected);
+    }
+
     fn check_second(input: &str, expected: usize) {
-        match second(input) {
-            Ok(result) => {
-                assert_eq!(result, expected);
-            }
-            Err(e) => {
-                for cause in e.causes() {
-                    println!("{}", cause);
-                }
-                panic!("test failed");
-            }
-        };
+        check(second(input), expected);
     }
 
     #[test]
@@ -136,5 +154,12 @@ mod tests {
     fn test_second() {
         let input = "p=<-6,0,0>, v=< 3,0,0>, a=< 0,0,0>\np=<-4,0,0>, v=< 2,0,0>, a=< 0,0,0>\np=<-2,0,0>, v=< 1,0,0>, a=< 0,0,0>\np=< 3,0,0>, v=<-1,0,0>, a=< 0,0,0>";
         check_second(input, 1);
+    }
+
+    use test::Bencher;
+    #[bench]
+    fn bench_second(b: &mut Bencher) {
+        let input = include_str!("../../data/d20-test");
+        b.iter(|| check_second(input, 471));
     }
 }
